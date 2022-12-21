924c274e34285a59f4fcf8365a0224554ee10688
JL <19100liu@gmail.com>
2022-12-21 12:12:56 -0500

    funish frame

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-title">Milly Liu</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About Me</a>
+          </li>
+          <li>
+            <a href="">Portfolio</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
